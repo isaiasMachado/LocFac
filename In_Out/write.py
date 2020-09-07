068604dc325cfadc3eafa0cb59aa45c5a1a693ae
Isaias <immgermano@gmail.com>
2020-09-07 19:32:26 -0300

    Ajuste do modo de escrita
    Mudado mode de escrita de 'a+' para 'w' e deletadas as pastas residuais do outro projeto.

--- In_Out/write.py
+++ In_Out/write.py
@@ -1,7 +1,7 @@
 
 def write_file(dados, name_instancia):
 
-    with open('Results/' + name_instancia + '.txt', mode='a+') as fp:
+    with open('Results/' + name_instancia + '.txt', mode='w') as fp:
         cont = 0
         for c in dados:
             print(cont, c, file=fp)
