2af432431f03ea64fca9b8018207842caaedc448
Denis Souza <denis.tracoleal@gmail.com>
2020-09-18 10:12:36 -0300

    #1 Ajuste no arquivo

--- In_Out/write.py
+++ In_Out/write.py
@@ -1,6 +1,6 @@
 
 def write_file(dados, name_instancia):
-   # print(dados)
+
 
     print(dados.get('nroFac'))
     print(dados.get('nroCli'))
@@ -9,13 +9,9 @@ def write_file(dados, name_instancia):
 
     #print(name_instancia)
     with open('Results/' + name_instancia + '.txt', mode='w') as fp:
-        cont = 0
+
         print('Qunatidade de Facilidades: '+ str( dados.get('nroFac')),
               'Quantidade de Cliente: '+ str(dados.get('nroCli')), file=fp)
 
-        # for c in dados:
-        #     print(c)
-        #     print(cont, c, file=fp)
-        #     cont += 1
 
 
