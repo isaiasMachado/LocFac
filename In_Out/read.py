1c630016f9d0b95b78e5ba49a0b0293b182b8f64
Isaias <immgermano@gmail.com>
2020-09-27 21:02:07 -0300

    Utilizando o pulp
    1- Alterado arquivo de leitura para ler instancia com as capacidades da facilidade.
    2- Alterado o main.py para receber os dicionários da instancia e enviá-los para o pulp.

--- In_Out/read.py
+++ In_Out/read.py
@@ -1,10 +1,13 @@
 import numpy as np
+import math
+
 
 def read_inst(nome):
-
-    facCapac = []
     facCust = []
+    facCapac = []
+    cusTransCli = []
     cliDema = []
+
     with open(nome, mode='r') as fp:
 
         tamInst = fp.readline().split()
@@ -14,9 +17,32 @@ def read_inst(nome):
         for f in range(0, facQtd):
             facInfo = fp.readline().split()
             facCapac.append(int(facInfo[0]))
-            facCust.append(int(facInfo[1]))
+            facCust.append(float(facInfo[1]))
 
-        matCt = np.genfromtxt(fp, dtype="float", max_rows=cliQtd)
-        inst_lidas = {'nroFac': facQtd, 'nroCli': cliQtd, 'demanda': matCt}
-    return inst_lidas
+        for d in range(cliQtd):
+            demanda = int(fp.readline().strip())
+            cliDema.append(demanda)
+            linhas = math.ceil(facQtd / 7)
+
+            custoClientes = []
+
+            for _ in range(linhas):
+                linha = fp.readline().strip().split()
+                custoClientes += linha
+
+            custoClientes = list([float(x) for x in custoClientes])
+
+            cusTransCli.append(custoClientes)
+
+    cusTransCli = np.array(cusTransCli).transpose()
+    print(cusTransCli)
+    cusTransCliFinal = []
+
+    for a in cusTransCli:
+        dictAux = {}
+        for index, l in enumerate(a):
+            dictAux[index + 1] = l
+        cusTransCliFinal.append(dictAux)
+
+    return facCust, facCapac, cusTransCliFinal, cliDema
 
